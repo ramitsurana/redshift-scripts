75d5cab2cc75b4cdbcf63bb7cbd10eb8f98ac8b9
Ramit Surana <ramitsurana@gmail.com>
2018-08-19 12:13:56 +0530

    Update s3.sql

--- COPY/s3.sql
+++ COPY/s3.sql
@@ -1,8 +1,8 @@
 -- Over SSH 
 -- S3 Manifest File - https://docs.aws.amazon.com/redshift/latest/dg/copy-parameters-data-source-ssh.html#copy-parameters-data-source-ssh-parameters
 
-copy "<REDSHIFT-TABLE-NAME>" from "s3://<MANIFEST-FILE-NAME>" credentials "arn:*:*" ssh;
+copy "<REDSHIFT-TABLE-NAME>" from "s3://<MANIFEST-FILE-NAME>" credentials "aws_iam_role=arn:*:*" ssh;
 
 -- Using S3
 
-copy "<REDSHIFT-TABLE-NAME>" from "s3://<S3-FILE-NAME>" credentials "arn:*:*";
+copy "<REDSHIFT-TABLE-NAME>" from "s3://<S3-FILE-NAME>" credentials "aws_iam_role=arn:*:*";
